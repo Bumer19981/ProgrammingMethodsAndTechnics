457e0489b38d784d09205ebbb8cd3537a19dcbb2
Andrew Andrew <bumer19981@gmail.com>
2020-04-12 19:38:09 +0700

    Исправлен enum

--- ProgrammingMethodsAndTechnics/objectoriented_Out.cpp
+++ ProgrammingMethodsAndTechnics/objectoriented_Out.cpp
@@ -6,14 +6,14 @@ namespace simple_langtypes {
     void Out(langtype::objectoriented& o, ofstream& ofst)
     {
         string inheritance;
-        if (o.inheritance == 0) {
+        if (o.inheritance == o.once) {
             inheritance = "once";
         }
-        else if (o.inheritance == 1) {
+        else if (o.inheritance == o.multiple) {
             inheritance = "multiple";
         }
         else {
-            inheritance = "inherit";
+            inheritance = "interface";
         }
         ofst << "It is an object-oriented language: inheritance = " << inheritance << ", year = " << o.year << endl;
     }
